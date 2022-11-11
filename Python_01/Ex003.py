8c4f3d9374f9cb40034ff26cfcf0ec704f8ae455
Jean Correa <jeanlucorrea@gmail.com>
2022-11-11 17:35:59 -0300

    Exercicio 3 Implementado

--- Python_01/Ex003.py
+++ Python_01/Ex003.py
@@ -1,6 +1,6 @@
 # Soma Números - Exercicios Huicode Academy
 
-soma_numeros = 0
+ soma_numeros = 0
 numero = int(input("Por favor, insira um número: "))
 for i in range(1, numero + 1, 1):
     soma_numeros += i
